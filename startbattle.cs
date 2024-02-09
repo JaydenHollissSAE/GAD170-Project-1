e6a699005c617c1b0954af19a7646676de85478a
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-02-09 13:27:52 +1100

    Tried porting to C#, needs more work

---
+++ startbattle.cs
@@ -0,0 +1,17 @@
+var rand = new Random();
+
+static void GetEnemyStats()
+{
+    public int tmpStats = Math.Round((rnd.NextDouble())*int(enemtLvl))
+    if (tmpStats < 1) {
+        tmpstats = 1
+    }
+}
+
+/*static void EnemyBattle()
+{
+
+
+}*/
+GetEnemyStats();
+global::System.Object value = console.WrittenLine(tmpStats);
