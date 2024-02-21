b74af8bc810c7882a1fc3d4ee67ddcdc1d0b2e2c
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-02-22 10:56:23 +1100

    Performed debugging and tests

--- Assets/StartBattle.cs
+++ Assets/StartBattle.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 /// </summary>
 public class StartBattle : MonoBehaviour
 {
-    GetEnemyStats GetEnemyStatsFunc;
+    //GetEnemyStats GetEnemyStatsFunc;
 
     public string enemyName;
     public string currentEnemy;
@@ -24,35 +24,43 @@ public class StartBattle : MonoBehaviour
 
     private void Start()
     {
+        playerLvl = 1;
         string[] enemyList = { "Test1", "Test2", "Test3" };
         StartBattleFunc();
         Debug.Log("Current Enemy: " + enemyName + " the " + currentEnemy);
-        Debug.Log("EnemyLvl: " + enemyLvl.ToString());
+        Debug.Log("EnemyLvl: " + enemyLvl);
         Debug.Log("Atk: " + enemyAtk);
-        Debug.Log("Def: " + enemyDef.ToString());
-        Debug.Log("Eva: " + enemyEva.ToString());
-        Debug.Log("Crt: " + enemyCrt.ToString());
-        Debug.Log("HP: " + enemyHP.ToString());
+        Debug.Log("Def: " + enemyDef);
+        Debug.Log("Eva: " + enemyEva);
+        Debug.Log("Crt: " + enemyCrt);
+        Debug.Log("HP: " + enemyHP);
     }
 
     public void StartBattleFunc()
     {
-        string enemyName = namesList[Random.Range(0, namesList.Length-1)];
-        string currentEnemy = enemyList[Random.Range(0, enemyList.Length - 1)];
-        int enemyLvl = Random.Range(playerLvl-6,playerLvl+1);
+        //string enemyName = namesList[Random.Range(0, namesList.Length - 1)];
+        //string currentEnemy = enemyList[Random.Range(0, enemyList.Length - 1)];
+        int enemyLvl = Random.Range(playerLvl-6, playerLvl+1);
         if (enemyLvl < 1) {
             enemyLvl = 1;
         }
         int tmpStats = 1;
-        GetEnemyStats.GetEnemyStatsFunc();
+        Debug.Log("Run Stats");
+        //GetEnemyStats.GetEnemyStatsFunc();
         int enemyAtk = tmpStats;
-        GetEnemyStats.GetEnemyStatsFunc();
+        //GetEnemyStats.GetEnemyStatsFunc();
         int enemyDef = tmpStats;
-        GetEnemyStats.GetEnemyStatsFunc();
+        //GetEnemyStats.GetEnemyStatsFunc();
         int enemyEva = tmpStats;
-        GetEnemyStats.GetEnemyStatsFunc();
+        //GetEnemyStats.GetEnemyStatsFunc();
         int enemyCrt = tmpStats;
-        GetEnemyStats.GetEnemyStatsFunc();
-        int enemyHP = (int)Mathf.Round((float)(tmpStats * 10.532976543));    
+        //GetEnemyStats.GetEnemyStatsFunc();
+        int enemyHP = (int)Mathf.Round((float)(tmpStats * 10.532976543));
+        Debug.Log("EnemyLvl: " + enemyLvl);
+        Debug.Log("Atk: " + enemyAtk);
+        Debug.Log("Def: " + enemyDef);
+        Debug.Log("Eva: " + enemyEva);
+        Debug.Log("Crt: " + enemyCrt);
+        Debug.Log("HP: " + enemyHP);
     }
 }
